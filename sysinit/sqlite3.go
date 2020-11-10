209a293ece0dac4b217b423674c8f074be66443f
TimHsu <maochindada@gmail.com>
2020-11-10 18:13:37 +0800

    init

--- sysinit/sqlite3.go
+++ sysinit/sqlite3.go
@@ -10,10 +10,10 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-// func init() {
-// 	InitMachineSettings()
-// 	InitSettings()
-// }
+func init() {
+	InitMachineSettings()
+	InitSettings()
+}
 
 // InitMachineSettings InitMachineSettings
 func InitMachineSettings() {
