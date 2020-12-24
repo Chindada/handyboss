41e3f77f015d65de566ca7ad3afe1bd3aacb492c
Tim Hsu <maochindada@gmail.com>
2020-12-24 21:04:13 +0800

    reinstall mac

--- handymodels/companymodel/company.go
+++ handymodels/companymodel/company.go
@@ -1,1 +1,10 @@
 package companymodel
+
+import "github.com/jinzhu/gorm"
+
+// Company Company
+type Company struct {
+	gorm.Model `json:"-"`
+	Number     string `gorm:"column:number;" json:"number"`
+	Name       string `gorm:"column:name;" json:"name"`
+}
